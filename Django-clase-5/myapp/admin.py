777bca2c8c7f0fe50558896874beb30fecf571a9
YezidBlanco <yezidblanco@gmail.com>
2024-07-23 17:01:39 -0500

    Implementando segundo modelo con clase clientes con Django

--- Django-clase-5/myapp/admin.py
+++ Django-clase-5/myapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from myapp.models import Person
+from myapp.models import Person, Customer
 
 # Register your models here.
 
 admin.site.register(Person)
-
+admin.site.register(Customer)
--- Django-clase-5/myapp/models.py
+++ Django-clase-5/myapp/models.py
@@ -7,4 +7,17 @@ class Person(models.Model):
     last_name = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.first_name
+        return f"{self.first_name} {self.last_name}"
+    
+class Customer(models.Model):
+    document_number = models.CharField(unique=True, max_length=10)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    birth_date = models.DateField()
+    residence_address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.document_number} {self.first_name} {self.last_name}"
+
