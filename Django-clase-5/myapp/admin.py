c8689a6c0d08e05d1cde5dd173396654d1d689b7
YezidBlanco <yezidblanco@gmail.com>
2024-07-23 22:48:53 -0500

    Implementando en el modelo la clase productos

--- Django-clase-5/myapp/admin.py
+++ Django-clase-5/myapp/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from myapp.models import Person, Customer
+from myapp.models import Person, Customer, Product
 
 # Register your models here.
 
-admin.site.register(Person)
-admin.site.register(Customer)
+class CurstomerAdmin(admin.ModelAdmin):
+    list_display = ["document_number", "first_name", "last_name"]
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ["id","document_number", "first_name", "last_name"]
+    search_fields = [ "document_number","first_name", "last_name"]
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id","product_code", "product_name", "brand", "model", "stock"]
+    search_fields = [ "first_name", "last_name"]
+    list_filter = ["brand"]
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Customer, CurstomerAdmin)
+admin.site.register(Product, ProductAdmin)
--- Django-clase-5/myapp/models.py
+++ Django-clase-5/myapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Person(models.Model):
+    document_number = models.CharField(unique=True, max_length=10)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
@@ -21,3 +22,17 @@ class Customer(models.Model):
     def __str__(self):
         return f"{self.document_number} {self.first_name} {self.last_name}"
 
+class Product(models.Model):
+    product_code = models.CharField(unique=True, max_length=10)
+    product_name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    serial_number = models.CharField(unique=True, max_length=20)
+    observations = models.TextField()
+    buy_price = models.IntegerField()
+    sell_price = models.IntegerField()
+    stock = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.product_code} {self.product_name} {self.brand} {self.model} {self.stock}"
+    
